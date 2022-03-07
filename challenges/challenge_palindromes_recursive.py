1d8c7d8b77cec12a5a6897e719b1db93010aa782
Junior Portela <portelajr14@gmail.com>
2022-03-07 00:57:16 -0300

    save commit: resolver caso da palindromo de tamanho 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -2,14 +2,15 @@ def is_palindrome_recursive(word, low_index, high_index):
 
     limit_iteractions = 0
 
-    if limit_iteractions == len(word) // 2:
+    if limit_iteractions == int(len(word) // 2):
         return True
 
     while limit_iteractions < len(word) // 2:
-        # if (high_index - low_index) < 2:
-        #     limit_iteractions = limit_iteractions + 1
-        #     return is_palindrome_recursive(word, low_index, high_index)
-        if word[low_index] == word[high_index]:
+        equality = word[low_index] == word[high_index]
+        if (high_index - low_index) <= 2 and equality:
+            limit_iteractions = limit_iteractions + 1
+            return is_palindrome_recursive(word, low_index, high_index)
+        if equality is True:
             limit_iteractions = limit_iteractions + 1
             return is_palindrome_recursive(word, low_index + 1, high_index - 1)
         else:
