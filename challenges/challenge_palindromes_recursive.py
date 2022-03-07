73b8df79945da0cef9b52faa324a552be5b1339a
Junior Portela <portelajr14@gmail.com>
2022-03-07 19:04:04 -0300

    feat req: funçao verifica palindromo em uma string a partir de primeiro e ultimo indices fornecidos

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -3,18 +3,13 @@ from math import ceil
 
 def is_palindrome_recursive(word, low_index, high_index):
 
-    # limit_iteractions = 0
+    if len(word) == 0:
+        return False
 
-    if low_index == ceil(len(word) / 2) or len(word) <= 2:
+    if low_index == ceil(len(word) / 2) or 0 < len(word) <= 2:
         return True
 
     while low_index < ceil(len(word) / 2):
-        # equality = word[low_index] == word[high_index]
-
-        # if len(word) <= 2:
-        #     limit_iteractions = limit_iteractions + 1
-        #     return is_palindrome_recursive(word, low_index, high_index)
-        # print(limit_iteractions)
         if word[low_index] == word[high_index]:
             # limit_iteractions = limit_iteractions + 1
             return is_palindrome_recursive(word, low_index + 1, high_index - 1)
