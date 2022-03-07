a492abdb5d756bd01647eeb876e98b1874c4b7f4
Junior Portela <portelajr14@gmail.com>
2022-03-06 23:21:41 -0300

    feat req1: funçao study_schedule verifica ocorrencia do valor inteiro entre os valores da lista de tuplas

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,25 +1,18 @@
-def checkTupla(tupla):
-    if isinstance(tupla[0], int) or isinstance(tupla[1], int) is False:
-        return None
-
-
 def study_schedule(permanence_period, target_time):
-    print("entrou algoritmo")
     if target_time is None:
         return None
 
     present_students = 0
 
     for student in permanence_period:
-        if checkTupla(student) is None:
+        if type(student[0]) is not int or type(student[1]) is not int:
             return None
-        print(student[0], student[1])
 
-        if (student[0] == target_time) or (target_time == student[1]) is True:
-            print("here!")
+        # print(target_time)
+        if (student[0] <= target_time <= student[1]) is True:
+            # print("here!")
             present_students = present_students + 1
     return present_students
-
 
 # https://stackoverflow.com/questions/32770725/how-to-check-a-specific-type-of-tuple-or-list
 # https://www.programiz.com/python-programming/methods/built-in/isinstance
