c4c2cb8ca11d357f0816b44cb3f4d60859c1c13a
Junior Portela <portelajr14@gmail.com>
2022-03-07 18:59:40 -0300

    save commit: req2 - falta apenas tratamento de entrada

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,17 +1,22 @@
+from math import ceil
+
+
 def is_palindrome_recursive(word, low_index, high_index):
 
-    limit_iteractions = 0
+    # limit_iteractions = 0
 
-    if limit_iteractions == int(len(word) // 2):
+    if low_index == ceil(len(word) / 2) or len(word) <= 2:
         return True
 
-    while limit_iteractions < len(word) // 2:
-        equality = word[low_index] == word[high_index]
-        if (high_index - low_index) <= 2 and equality:
-            limit_iteractions = limit_iteractions + 1
-            return is_palindrome_recursive(word, low_index, high_index)
-        if equality is True:
-            limit_iteractions = limit_iteractions + 1
+    while low_index < ceil(len(word) / 2):
+        # equality = word[low_index] == word[high_index]
+
+        # if len(word) <= 2:
+        #     limit_iteractions = limit_iteractions + 1
+        #     return is_palindrome_recursive(word, low_index, high_index)
+        # print(limit_iteractions)
+        if word[low_index] == word[high_index]:
+            # limit_iteractions = limit_iteractions + 1
             return is_palindrome_recursive(word, low_index + 1, high_index - 1)
         else:
             return False
