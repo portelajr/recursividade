3938abea2063c947b1174d80271e3760554bd7ed
Junior Portela <portelajr14@gmail.com>
2022-03-10 18:11:46 -0300

    feat req5: funcao is_palindrome_iterative recebe string e retorna boolean

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -4,9 +4,9 @@
 def is_palindrome_iterative(word):
     min_index = 0
     max_index = len(word) - 1
-    # print(min_index, max_index)
-    # limit_iteractions = ceil(len(word) / 2)
-    # print(limit_iteractions)
+
+    if len(word) == 0:
+        return False
 
     while min_index < max_index:
         if word[min_index] != word[max_index]:
@@ -14,20 +14,3 @@ def is_palindrome_iterative(word):
         min_index = min_index + 1
         max_index = max_index - 1
     return True
-
-
-# print(is_palindrome_iterative("ANA"))
-
-# def is_palindrome_recursive(word, low_index, high_index):
-
-#     if len(word) == 0:
-#         return False
-
-#     if low_index == ceil(len(word) / 2) or 0 < len(word) <= 2:
-#         return True
-
-#     while low_index < ceil(len(word) / 2):
-#         if word[low_index] == word[high_index]:
-#             return is_palindrome_recursive(word, low_index + 1, high_index - 1)
-#         else:
-#             return False
